aa95fc214d8ba9349eb15b8d7d7f58a595033424
hafeezarif <108301613+hafeezarif@users.noreply.github.com>
2022-10-16 14:19:24 -0400

    Added Now Playing Section

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,32 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+const collapsibleToggle = document.querySelector("#collapsible-toggle");
+const collapsibleContent = document.querySelector("#collapsible-content");
+const nowPlayingGrid = document.querySelector(".now-playing-grid");
 
-const collapsibleToggle = document.querySelector('#collapsible-toggle');
-const collapsibleContent = document.querySelector('#collapsible-content');
+const MOVIE_DB_API_KEY = "ef36b87931742344a95578159a02b2d1";
+const nowPlayingMoviesURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${MOVIE_DB_API_KEY}&language=en-US&page=1`;
 
-collapsibleToggle.addEventListener('click', function() {
-  collapsibleContent.classList.toggle('hidden');
-})
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(nowPlayingMoviesURL).then((response) => {
+    response.json().then((data) => {
+      data.results.forEach((movie) => {
+        const movieImageURL = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`
+        const movieCardTemplate = `<div class="now-playing-card">
+        <img src="${movieImageURL}" alt="${movie.title} poster" class="now-playing-img">
+        <h3 class="now-playing-title">${movie.title}</h3>
+        </div>`;
+        nowPlayingGrid.innerHTML += movieCardTemplate;
+      });
+    });
+  });
+});
+
+collapsibleToggle.addEventListener("click", () => {
+  collapsibleContent.classList.toggle("hidden");
+});
+
+// search
+//https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=W7EYlsCUiNLdyGGchcLIaPQx0iaAkkua
+// Posters
+//https://api.themoviedb.org/3/movie/550?api_key=ef36b87931742344a95578159a02b2d1
+// now playing
+//https://api.themoviedb.org/3/movie/now_playing?api_key=ef36b87931742344a95578159a02b2d1&language=en-US&page=1
