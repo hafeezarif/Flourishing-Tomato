9c27270b71dadf10305c0b688ec3e310595cadd1
hafeezarif <108301613+hafeezarif@users.noreply.github.com>
2022-10-19 17:34:06 -0400

    Fixed Movie.js for Bulma

--- assets/movie.js
+++ assets/movie.js
@@ -11,11 +11,10 @@ fetch(MovieReviewURL).then((response) => {
   response.json().then((data) => {
     console.log(data);
     movieSection.innerHTML = `
-        <div class="is-align-items-center>
-        <h2 <div class="block is-size-2"> ${title}</h2>
-        <span <div class="block is-size-6"> ${release_date}</span>
-        <p <div class="block has-text-right"> ${overview}</p>
-        <a href="${data.results[0]?.link.url}" target="_blank">Read Review</a> 
-        <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" />`;
+        <h2 class="has-text-danger-dark has-text-weight-bold is-size-2 ml-4 mt-4"> ${title}</h2>
+        <span class="pl-4 has-text-weight-semibold has-text-danger-dark is-size-4"> ${release_date}</span>
+        <a class="is-size-4 pl-1" href="${data.results[0]?.link.url}" target="_blank">Read Review</a> 
+        <p class="has-text-left has-text-danger-dark pl-4 is-size-5 mb-4 mt-2"> ${overview}</p>
+        <img class="ml-4 " src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" />`;
   });
 });
